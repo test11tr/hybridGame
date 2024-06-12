b6169d48cc17b47e3176822cc6098c31518d05ae
test11 <kaantekin67@gmail.com>
2024-06-12 23:46:21 +0300

    get rid of characterController package and do it yourself boi

---
+++ Assets/Game/Scripts/CharacterControlManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharacterControlManager : MonoBehaviour
+{
+    public T11Joystick joystick;
+    public CharacterController controller;
+    public Animator playerAnimator;
+    public Canvas inputCanvas;
+    public bool isJoytick;
+
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        EnableJoystick();
+    }
+
+    public void EnableJoystick()
+    {
+        isJoytick = true;
+        inputCanvas.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (isJoytick)
+        {
+            var movementDirection = new Vector3(joystick.Direction.x, 0, joystick.Direction.y);
+            controller.SimpleMove(movementDirection * movementSpeed);
+
+            if(movementDirection.sqrMagnitude <= 0)
+            {
+                playerAnimator.SetBool("isWalking", false);
+                return;
+            }
+
+            playerAnimator.SetBool("isWalking", true);
+            var targetDirection = Vector3.RotateTowards(controller.transform.forward, movementDirection, rotationSpeed * Time.deltaTime, 0.0f);
+            controller.transform.rotation = Quaternion.LookRotation(targetDirection);
+        }
+    }
+}
